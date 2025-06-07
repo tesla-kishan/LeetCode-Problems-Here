89c90a5e8980e71ec74c26d34d326443a74dcece
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-06-07 15:23:41 +0530

    Time: 0 ms (100%), Space: 44.9 MB (8.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,18 +14,20 @@
  * }
  */
 class Solution {
-    public int height(TreeNode root){
-        if (root == null) return 0;
-        int left = height(root.left);
-        int right = height(root.right);
-        if (left == -1 || right == -1) return -1;
-        if (Math.abs(left-right) >1) return -1;
-        return  Math.max(left,right) + 1;
-    }
+    int  valid = 1;
     public boolean isBalanced(TreeNode root) {
-        int ht = height(root);
-        if (ht == -1) return false;
+        if(root == null) return true;
+        fn(root);
+        if(valid == 0) return false;
         return true;
-        
+    }
+    public int fn(TreeNode root){
+        if(root == null) return 0;
+        int left = fn(root.left);
+        int right = fn(root.right);
+        if(Math.abs(left-right)>1) {
+            valid = 0;
+        }
+        return Math.max(left,right)+1;
     }
 }
