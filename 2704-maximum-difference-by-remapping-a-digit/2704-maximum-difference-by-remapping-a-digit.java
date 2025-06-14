75ad10b3bf546d58d139b1ae16297be26db6b91e
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-06-14 17:48:53 +0530

    Time: 1 ms (89.2%), Space: 40.1 MB (97.73%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
@@ -1,43 +1,23 @@
 class Solution {
     public int minMaxDifference(int num) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        int original = num;
-        while(num>0){
-            int ld = num%10;
-            ans.add(ld);
-            num /= 10;
-        }
-        Collections.reverse(ans);
-        ArrayList<Integer> maxlist = new ArrayList<>(ans);
-        ArrayList<Integer> minlist = new ArrayList<>(ans);
-        int x = -1;
-        for(int d : maxlist){
-            if(d != 9){
-                x=d;
+       // String str1 = String.valueOf(num);  
+        String str1 = Integer.toString(num);  
+        String str2 = str1;
+
+        int idx = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != '9') {
+                idx = i;
                 break;
             }
         }
-        if(x != -1){
-        for(int i=0 ; i<ans.size() ; i++){
-            if(maxlist.get(i)==x){
-                maxlist.set(i,9);
-            }
+
+        if (idx < str1.length()) {
+            str1 = str1.replace(str1.charAt(idx), '9');
         }
-        }
-        int largenum = 0;
-        for(int digits : maxlist){
-            largenum = largenum * 10 + digits;
-        }
-        int y = minlist.get(0);
-        for(int i=0 ; i<minlist.size() ; i++){
-            if(minlist.get(i)==y ){
-                minlist.set(i,0);
-            }
-        }
-        int smallnum = 0;
-        for(int digits : minlist){
-            smallnum = smallnum * 10 + digits;
-        }
-        return largenum-smallnum;
+
+        str2 = str2.replace(str2.charAt(0), '0');
+
+        return Integer.parseInt(str1) - Integer.parseInt(str2);
     }
 }
