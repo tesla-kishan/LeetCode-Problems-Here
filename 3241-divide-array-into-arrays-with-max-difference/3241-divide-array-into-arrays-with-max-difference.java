79e0c06fd69fabee34f780486525d2e162fa306e
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-06-19 13:28:38 +0530

    Time: 24 ms (35.18%), Space: 58.9 MB (86.43%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
@@ -1,24 +1,18 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
-       ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-       int n = nums.length;
-       Arrays.sort(nums);
-       for(int i=0 ; i<n ; i=i+3){
-        if(nums[i+2]-nums[i]>k){
-            return new int[0][0];
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans[][] = new int[n/3][3];
+        int idx=0;
+        for(int i=0 ; i<n ;i+=3){
+           if(nums[i+2]-nums[i] >k){
+                return new int[0][0];
+            }
+            ans[idx][0] = nums[i];
+            ans[idx][1] = nums[i+1];
+            ans[idx][2] = nums[i+2];
+            idx++;
         }
-        ArrayList<Integer> temp = new ArrayList<>();
-        temp.add(nums[i]);
-        temp.add(nums[i+1]);
-        temp.add(nums[i+2]);
-        ans.add(temp);
-       }
-       int res[][] = new int [ans.size()][3];
-       for(int i=0 ;i<ans.size() ; i++){
-        for(int j=0 ; j<3 ; j++){
-            res[i][j] = ans.get(i).get(j);
-        }
-       }
-       return res;
+        return ans;
     }
 }
