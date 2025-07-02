ceeb25efbde4237a8def6df0fcb31ba4cdf80b77
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-02 19:44:16 +0530

    Time: 0 ms (100%), Space: 41.9 MB (53.98%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,16 +14,26 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return fn(root.left,root.right);
-    }
-    public boolean fn(TreeNode p , TreeNode q){
+    public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null) return true;
-        if(p==null && q != null) return false;
-        if(p!=null && q == null) return false;
+        if(p==null && q!=null) return false;
+        if(p!=null && q==null) return false;
         if(p.val != q.val) return false;
-        boolean ans1 = fn(p.left,q.right);
-        boolean ans2 = fn(p.right,q.left);
-        return ans1 && ans2;
+        boolean left = isSameTree(p.left,q.left);
+        boolean right = isSameTree(p.right,q.right);
+        return left && right;
     }
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return root;
+        TreeNode l = root.left;
+        TreeNode r = root.right;
+        root.left = invertTree(r);
+        root.right = invertTree(l);
+        return root;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        TreeNode x = invertTree(root.left);
+        return isSameTree(x,root.right);
+    }
+    
 }
