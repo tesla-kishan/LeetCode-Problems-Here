99996aaf7ca5d3f30d8be8a94ac6f90dc6f7fd0b
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-02 14:39:57 +0530

    Time: 8 ms (38.61%), Space: 42.7 MB (65.09%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -16,16 +16,16 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> ans = new ArrayList<>();
-        fn(ans,root,"");
+        traverse(root,ans,"");
         return ans;
+
     }
-    public void fn(List<String> ans, TreeNode root , String s){
-        if(root ==null) return;
+    public void traverse(TreeNode root , List<String> ans , String s){
+        if (root == null) return;
         if(root.left == null && root.right == null){
             ans.add(s+root.val);
-            return;
         }
-        fn(ans,root.left,s+root.val+"->");
-        fn(ans,root.right,s+root.val+"->");
+        traverse(root.left,ans,s+root.val+"->");
+        traverse(root.right,ans,s+root.val+"->");
     }
 }
