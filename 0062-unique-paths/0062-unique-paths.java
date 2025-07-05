a9ef1bf5a3cb7f52ade091ee896b3a4d4a532895
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-05 10:31:33 +0530

    Time: 0 ms (100%), Space: 40.8 MB (25.47%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int Paths(int m, int n , int dp[][]) {
+        if(m==1 && n==1) return 1;
+        if(m==0 || n==0) return 0;
+        if(dp[m][n] != -1) return dp[m][n];
+        int rightways = Paths(m,n-1,dp);
+        int downways = Paths(m-1,n,dp);
+        return dp[m][n] = rightways+downways;
+    }
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int [m+1][n+1];
+        for(int i=1; i<=m ; i++){
+            for(int j=0 ; j<=n ;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return Paths(m,n,dp);
+    }
+}
