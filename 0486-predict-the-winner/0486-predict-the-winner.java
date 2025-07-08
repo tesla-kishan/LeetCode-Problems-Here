8ba4bbd18a55cca385848a6a0f7fdad89a240c37
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-08 16:20:21 +0530

    Time: 67 ms (28.72%), Space: 40.9 MB (76.53%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,23 +1,13 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        int dp[][] = new int[n][n];
-        for(int row[]: dp) Arrays.fill(row,-1);
-        int sum = Arrays.stream(nums).sum();
-        int player1 = solve(0,n-1,nums,dp);
-        int player2 = sum - player1;
-        return player1>=player2;
+        return solve(0,n-1,nums)>=0;
     }
-    public int solve(int i , int j , int nums[],int dp[][]){
+    public int solve(int i, int j , int nums []){
         if(i>j) return 0;
         if(i==j) return nums[i];
-        if (dp[i][j] != -1) return dp[i][j];
-        int take_i = nums[i] + Math.min(solve(i+1,j-1,nums,dp),solve(i+2,j,nums,dp));
-        int take_j = nums[j] + Math.min(solve(i+1,j-1,nums,dp),solve(i,j-2,nums,dp));
-        return dp[i][j] = Math.max(take_i,take_j) ;
+        int take_i = nums[i] - solve(i+1,j,nums);
+        int take_j = nums[j] - solve(i,j-1,nums);
+        return Math.max(take_i,take_j);
     }
 }
-
-
-//tc 2 power n
-// memoisation
