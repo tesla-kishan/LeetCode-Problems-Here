d68eaa1195e7f6914069a9baeb2ae9b16fa52175
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-08 16:23:20 +0530

    Time: 0 ms (100%), Space: 41.3 MB (38.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        return solve(0,n-1,nums)>=0;
+        int dp[][] = new int [n][n];
+        for(int [] row: dp) Arrays.fill(row,-1);
+        return solve(0,n-1,nums,dp)>=0;
     }
-    public int solve(int i, int j , int nums []){
+    public int solve(int i, int j , int nums [],int dp[][]){
         if(i>j) return 0;
         if(i==j) return nums[i];
-        int take_i = nums[i] - solve(i+1,j,nums);
-        int take_j = nums[j] - solve(i,j-1,nums);
-        return Math.max(take_i,take_j);
+        if(dp[i][j] != -1) return dp[i][j];
+        int take_i = nums[i] - solve(i+1,j,nums,dp);
+        int take_j = nums[j] - solve(i,j-1,nums,dp);
+        return dp[i][j] = Math.max(take_i,take_j);
     }
 }
