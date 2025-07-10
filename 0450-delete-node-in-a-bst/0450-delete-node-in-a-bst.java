9ddae66fc9ca04aaf93d28144d5fa0a57970afdf
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-11 00:56:44 +0530

    Time: 0 ms (100%), Space: 46.3 MB (35.13%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,60 +15,28 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        TreeNode temp = new TreeNode(Integer.MAX_VALUE);
-        temp.left=root;
-        delete(temp,key);
-        root = temp.left;
+        if(root==null) return root;
+        if(root.val>key) root.left = deleteNode(root.left,key);
+        else if(root.val<key) root.right = deleteNode(root.right,key);
+        else{
+            if(root.left==null) return root.right;
+            else if (root.right==null) return root.left;
+            else{
+                root.val = findmin(root.right);
+                root.right = deleteNode(root.right,root.val);
+            }
+        }
         return root;
     }
-    public void delete(TreeNode root, int key){
-        if(root==null) return;
-        if(root.val>key){ //go left
-            if(root.left==null) return;
-            if(root.left.val==key){
-                TreeNode l = root.left; // l isthe node we wish to delete
-                if(l.left ==null && l.right ==null){ // 0 child
-                    root.left=null;
-                }
-                else if (l.left==null || l.right==null){ // 1 child
-                    if(l.left!=null) root.left=l.left;
-                    else root.left=l.right;
-                }
-                else{ //2 child
-                    TreeNode curr = l;
-                    TreeNode pred = curr.left;
-                    while(pred.right != null) pred=pred.right;
-                    delete(root,pred.val);
-                    pred.left = curr.left;
-                    pred.right = curr.right;
-                    root.left = pred;
-                }
-            }
-            else delete(root.left,key);
+    public int findmin(TreeNode root){
+        TreeNode temp = root;
+        int min = root.val;
+        while(temp.left!=null){
+            temp = temp.left;
+            min = temp.val;
         }
-        else{
-            if(root.right==null) return;
-            if(root.right.val==key){
-                TreeNode r = root.right;
-                if(r.left ==null && r.right==null){
-                    root.right=null;
-                }
-                else if (r.left==null || r.right==null){
-                    if(r.left!=null) root.right= r.left;
-                    else root.right = r.right;
-                }
-                else{
-                    TreeNode curr = r;
-                    TreeNode pred = curr.left;
-                    while(pred.right != null) pred=pred.right;
-                    delete(root,pred.val);
-                    pred.left = curr.left;
-                    pred.right = curr.right;
-                    root.right = pred;
-
-                }
-            }
-            else delete(root.right,key);
-        }
+        return min;
     }
+    
+
 }
