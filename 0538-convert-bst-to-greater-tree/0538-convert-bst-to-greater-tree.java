801de999f281e9ba89a74388f183e7dc65f9d49c
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-16 22:42:11 +0530

    Time: 0 ms (100%), Space: 45.4 MB (48.83%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -14,17 +14,16 @@
  * }
  */
 class Solution {
-    static int sum =0;
-    public void helper(TreeNode root){
+    public void helper(TreeNode root ,int sum[]){
         if(root==null) return;
-        helper(root.right);
-        root.val = root.val+sum;
-        sum = root.val;
-        helper(root.left);
+        helper(root.right,sum);
+        root.val = root.val+sum[0];
+        sum[0] = root.val;
+        helper(root.left,sum);
     }
     public TreeNode convertBST(TreeNode root) {
-        sum=0;
-        helper(root);
+        int sum[] = new int [1];
+        helper(root,sum);
         return root;
     }
 }
