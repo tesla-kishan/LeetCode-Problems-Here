fac8bdacd6128aabfe2807e30775100520bdceee
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-16 23:05:25 +0530

    Time: 0 ms (100%), Space: 41.1 MB (80.43%) - LeetHub

---
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDiffInBST(TreeNode root) {
+         List<Integer> ans = new ArrayList<>();
+        inorder(root,ans);
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 1; i < ans.size(); i++) {
+            minDiff = Math.min(minDiff, ans.get(i) - ans.get(i-1));
+        }
+        return minDiff;
+        
+    }
+    public void inorder(TreeNode root ,List<Integer> ans){
+        if(root==null) return;
+        inorder(root.left,ans);
+        ans.add(root.val);
+        inorder(root.right,ans);
+    }
+}
