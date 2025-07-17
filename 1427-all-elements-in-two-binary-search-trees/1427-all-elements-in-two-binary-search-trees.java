83d6125d52fd47471765cdf5e7b84c0ec7b11b94
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-17 11:06:49 +0530

    Time: 20 ms (26.45%), Space: 46.3 MB (52.39%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
@@ -15,17 +15,76 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> a = new ArrayList<>();
+        TreeNode curr = root1;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr) pred = pred.right;
+                if(pred.right == null){
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                if(pred.right == curr){
+                    pred.right = null;
+                    a.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+            else{
+                a.add(curr.val);
+                curr=curr.right;
+            }
+        }
+        List<Integer> b = new ArrayList<>();
+        curr = root2;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr) pred = pred.right;
+                if(pred.right == null){
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                if(pred.right == curr){
+                    pred.right = null;
+                    b.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+            else{
+                b.add(curr.val);
+                curr=curr.right;
+            }
+        }
         List<Integer> ans = new ArrayList<>();
-        inorder(root1,ans);
-        inorder(root2,ans);
-        Collections.sort(ans);
+        merge(a,b,ans);
         return ans;
+    }
+    public void merge(List<Integer> a , List<Integer> b , List<Integer> ans){
+        int i =0 , j=0;
+        while(i<a.size() && j<b.size()){
+            if(a.get(i)<b.get(j)){
+                ans.add(a.get(i));
+                i++;
+            }
+            else{
+                ans.add(b.get(j));
+                j++;
+            }
+        }
+        if(i==a.size()){
+            while(j<b.size()){
+                ans.add(b.get(j));
+                j++;
+            }
+        }
+        if(j==b.size()){
+            while(i<a.size()){
+                ans.add(a.get(i));
+                i++;
+            }
+        }
+    }
 
-    }
-    public void inorder(TreeNode root ,List<Integer> ans){
-        if(root==null) return;
-        inorder(root.left,ans);
-        ans.add(root.val);
-        inorder(root.right,ans);
-    }
 }
