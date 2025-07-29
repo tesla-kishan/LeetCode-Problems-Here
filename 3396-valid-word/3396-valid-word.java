dae4a0aba6a89f26d34b32bbc77cd9fc67b9d5be
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-29 12:12:38 +0530

    Time: 1 ms (98.62%), Space: 41.9 MB (77.39%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isValid(String word) {
         if(word.length()<3) return false;
-        int vowel = 0, consonant=0;
+        boolean v=false, c=false;
         for(int i=0 ; i<word.length() ; i++){
             char ch = word.charAt(i);
             if(!Character.isLetterOrDigit(ch)) return false;
             if(Character.isLetter(ch)){
-                char lowerch = Character.toLowerCase(ch);
-                if(lowerch == 'a' || lowerch == 'e' || lowerch == 'i' || lowerch == 'o' || lowerch == 'u') vowel++;
-                else consonant++;
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') v=true;
+                else c=true;
             }
         }
-        return vowel>0 && consonant>0;
+        return v && c;
     }
 }
