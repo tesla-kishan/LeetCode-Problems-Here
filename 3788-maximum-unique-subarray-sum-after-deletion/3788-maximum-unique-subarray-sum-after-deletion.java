af0f820bfe6abd26df9b49dfa90a5dcea78da524
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-07-29 11:27:25 +0530

    Time: 1 ms (99.89%), Space: 43.2 MB (5.73%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxSum(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        int freq [] = new int[201];
         int sum=0 , maxneg=Integer.MIN_VALUE;
         for(int x: nums){
             if(x<=0) maxneg=Math.max(maxneg,x);
-            else if(!set.contains(x)){
+            else if(freq[x]==0){
                 sum+=x;
-                set.add(x);
+                freq[x]=1;
             }
         }
         return sum==0 ? maxneg : sum;
