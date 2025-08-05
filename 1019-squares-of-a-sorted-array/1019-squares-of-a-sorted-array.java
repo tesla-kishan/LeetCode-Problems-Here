212b53993d7f38a2eeceb1e824e63ce3e7728e43
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-08-05 22:27:13 +0530

    Time: 1 ms (100%), Space: 47.5 MB (34.64%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -3,7 +3,21 @@ class Solution {
         for(int i=0 ;i<nums.length ;i++){
             nums[i]=nums[i]*nums[i];
         }
-        Arrays.sort(nums);
-        return nums;
+        int low=0,high=nums.length-1;
+        int arr[] = new int [nums.length];
+        int idx=nums.length-1;
+        while(low<=high){
+            if(nums[low]>nums[high]){
+                arr[idx]=nums[low];
+                idx--;
+                low++;
+            }
+            else{
+                arr[idx]=nums[high];
+                idx--;
+                high--;
+            }
+        }
+        return arr;
     }
 }
