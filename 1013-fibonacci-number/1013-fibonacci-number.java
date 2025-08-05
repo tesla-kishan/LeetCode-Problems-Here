5164223dcebdb4778a40a822075a1177ff48fbf9
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-08-05 22:34:20 +0530

    Time: 0 ms (100%), Space: 40.4 MB (63.82%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,12 @@
 class Solution {
     public int fib(int n) {
+        int dp[] = new int [n+1];
+        Arrays.fill(dp,-1);
+        return fibo(n,dp);
+    }
+    public int fibo(int n,int dp[]){
         if(n<2) return n;
-        return fib(n-1)+fib(n-2);
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fibo(n-1,dp)+fibo(n-2,dp);
     }
 }
