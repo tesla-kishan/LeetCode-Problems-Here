31544067b1ae9d1a2ce64647f0b53c34ff4121a3
Kishan Lal <114351086+tesla-kishan@users.noreply.github.com>
2025-08-06 11:46:46 +0530

    Time: 11 ms (11.55%), Space: 46.6 MB (72.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Stack<int[]> stack = new Stack<>();
-        Arrays.sort(intervals , (a,b)-> a[0]-b[0]);
-        for (int[] interval : intervals) {
-            if (stack.isEmpty() || stack.peek()[1] < interval[0]) {
-                stack.push(interval);
-            } else {
-                stack.peek()[1] =Math.max(stack.peek()[1],interval[1]);
+        Arrays.sort(intervals,(a,b)-> a[0]-b[0]);
+        Stack<int[]> st = new Stack<>();
+        for(int interval[]:intervals){
+            if(st.isEmpty() || st.peek()[1] < interval[0]){
+                st.push(interval);
+            }else{
+                st.peek()[1] = Math.max(st.peek()[1],interval[1]);
             }
         }
-        return stack.toArray(new int[stack.size()][]);
+        return st.toArray(new int [st.size()][]);
+
     }
 }
